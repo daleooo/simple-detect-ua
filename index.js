8c71a98d736f4ba42062fdcee03daa33d936e117
张弛 <zhangchi07@meituan.com>
2016-03-15 19:48:45 +0800

    Add version

--- index.js
+++ index.js
@@ -9,54 +9,44 @@
     , FLASH_MIME_TYPE = "application/x-shockwave-flash"
     , EXPRESS_INSTALL_ID = "SWFObjectExprInst"
     , ON_READY_STATE_CHANGE = "onreadystatechange"
-
     , win = window
     , doc = document
     , nav = navigator
-
     , plugin = false;
 
-  var getBrowserName = function () {
-    var isChrome = navigator.userAgent.indexOf('Chrome') > -1;
-    var isExplorer = navigator.userAgent.indexOf('MSIE') > -1;
-    var isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
-    var isSafari = navigator.userAgent.indexOf("Safari") > -1;
-    var isOpera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
-    if ((isChrome)&&(isSafari)) {isSafari=false;}
-    if ((isChrome)&&(isOpera)) {isChrome=false;}
-    if (isChrome) return 'Chrome';
-    if (isExplorer) return 'MSIE';
-    if (isFirefox) return 'Firefox';
-    if (isSafari) return 'Safari';
-    if (isOpera) return 'Opera';
-  };
 
-  var getMobileOperatingSystem = function () { 
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera; 
-    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) { 
-      return 'IOS'; 
-    } 
-    else if(userAgent.match(/Android/i)) { 
-      return 'Android'; 
-    } 
-    else { 
-      return 'unknown'; 
-    } 
+  var parseVersion = function (key, text) {
+    var pattern = '^.*'+ key +'[\\/\\s](\\d+(\\.\\d+)?).*$';
+    var regex = new RegExp(pattern, 'i');
+    return text.replace(regex, '$1');
   }
 
   var ua = function () {
     var w3cdom = typeof doc.getElementById !== UNDEF && typeof doc.getElementsByTagName !== UNDEF && typeof doc.createElement !== UNDEF,
     u = nav.userAgent.toLowerCase(),
     p = nav.platform.toLowerCase(),
-    windows = p ? /win/.test(p) : /win/.test(u),
-    mac = p ? /mac/.test(p) : /mac/.test(u),
-    webkit = /webkit/.test(u) ? parseFloat(u.replace(/^.*webkit\/(\d+(\.\d+)?).*$/, "$1")) : false, // returns either the webkit version or false if not webkit
-    ie = nav.appName === "Microsoft Internet Explorer",
+    windows = p ? /win/.test(p)     : /win/.test(u),
+    linux   = p ? /linux/.test(p)   : /linux/.test(u),
+    mac     = p ? /mac/.test(p)     : /mac/.test(u),
+    iphone  = p ? /iphone/.test(p)  : /iphone/.test(u),
+    ipad    = p ? /ipad/.test(p)    : /ipad/.test(u),
+    ipod    = p ? /ipod/.test(p)    : /ipod/.test(u),
+    android = /android/.test(u) ? parseVersion('android', u) : false,
+    chrome  = /chrome/.test(u)  ? parseVersion('chrome', u) : false,
+    firefox = /firefox/.test(u) ? parseVersion('firefox', u) : false,
+    safari  = /safari/.test(u)  ? parseVersion('safari', u) : false,
+    opera   = /op/.test(u)      ? parseVersion('op', u) : false,
+    webkit  = /webkit/.test(u) ? parseVersion('webkit', u) : false,
+    trident = /trident/.test(u) ? parseVersion('trident', u) : false,
+    ie = nav.appName === "Microsoft Internet Explorer" || /msie/.test(u) || /trident/.test(u),
     playerVersion = [0, 0, 0],
     d = null;
 
-    console.log(u);
-    console.log(p);
+    if (chrome && safari)
+      safari = false;
+
+    if (chrome && opera)
+      chrome = false;
 
     if (typeof nav.plugins !== UNDEF && typeof nav.plugins[SHOCKWAVE_FLASH] === OBJECT) {
       d = nav.plugins[SHOCKWAVE_FLASH].description;
@@ -84,12 +74,57 @@
       }
       catch (e) {}
     }
-    return {w3: w3cdom, pv: playerVersion, wk: webkit, ie: ie, win: windows, mac: mac};
+ 
+    var pv = playerVersion;
+    var flash = true;
+    if (pv[0] == 0 && pv[1] == 0 && pv[2] == 0) {
+      flash = false; 
+    }
+    return {
+      w3       : w3cdom, 
+      flashVer : pv, 
+      flash    : flash, 
+      windows  : windows, 
+      mac      : mac, 
+      linux    : linux,
+      iphone   : iphone, 
+      ipad     : ipad,
+      ipod     : ipod,
+      android  : android,
+      chrome   : chrome,
+      firefox  : firefox,
+      safari   : safari,
+      opera    : opera,
+      ie       : ie, 
+      trident  : trident,
+      webkit   : webkit, 
+    };
   };
-  console.log(ua());
+  var userAgent = ua();
+  var flashVersion = userAgent.flashVer[0] + '.' + userAgent.flashVer[1] + '.' + userAgent.flashVer[2];
 
-  $('#container').append('<pre>'+ navigator.userAgent +'</pre>');
-  $('#container').append('<pre>'+ getBrowserName() +'</pre>');
-  $('#container').append('<pre>'+ getMobileOperatingSystem() +'</pre>');
-  $('#container').append('<pre>'+ ua() +'</pre>');
+  $('.container').append('<pre>'+ navigator.userAgent +'</pre>');
+  $('.container').append('<pre>'+ navigator.platform +'</pre>');
+
+  var data = {
+    w3c     : userAgent.w3,
+    mac     : userAgent.mac,
+    linux   : userAgent.linux,
+    windows : userAgent.windows,
+    ie      : userAgent.ie,
+    andorid : userAgent.android,
+    iphone  : userAgent.iphone,
+    webkit  : userAgent.webkit,
+    trident : userAgent.trident,
+    chrome  : userAgent.chrome,
+    firefox : userAgent.firefox,
+    safari  : userAgent.safari,
+    opera   : userAgent.opera,
+    flash   : userAgent.flash,
+    "flash version" : flashVersion,
+  }
+
+  for (var key in data) {
+    $('.tbody').append('<tr><td>' + key + '</td>' + '<td>' + data[key] +'</td></tr>');
+  }
 }())
